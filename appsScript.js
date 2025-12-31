5bd1914add990253aca458e6246cc51e84025a81
Александр Прокудин <aleksandrprokudin@Noutbuk-Aleksandr.local>
2025-12-31 10:42:17 +0700

    Trigger workflow: test --auth flag

--- appsScript.js
+++ appsScript.js
@@ -2,7 +2,7 @@
  * Creates the add-on menu when the spreadsheet is opened.
  * This function is automatically called by Google Sheets.
  * 
- * Last updated: 2024-12-30 - Testing improved authentication
+ * Last updated: 2024-12-30 - Testing --auth flag
  */
 function onOpen() {
   SpreadsheetApp.getUi()
